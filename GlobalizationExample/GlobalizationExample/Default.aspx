5d969eb3246734856e4d5ad1d0f10787f1d4f0cd
WillianDeCastro <oilha2010@hotmail.com>
2019-07-03 01:43:48 -0300

    Subida

---
+++ GlobalizationExample/GlobalizationExample/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GlobalizationExample._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+
+    <div>
+
+        <h1>Pagina inicial</h1>
+        <asp:Label ID="traducao" runat="server" Text="Label"></asp:Label>
+    </div>
+    <asp:ListBox ID="ListBox1" runat="server" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" AutoPostBack="true">
+        <asp:ListItem Value="pt-BR" Selected="True"></asp:ListItem>
+        <asp:ListItem Value="es-ES"></asp:ListItem>
+    </asp:ListBox>
+
+</asp:Content>
---
+++ GlobalizationExample/GlobalizationExample/Default.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GlobalizationExample
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+      
+ 
+        }
+
+        /// <summary>
+        /// Pode ser utilizado para setar o padrão do idioma na página
+        /// </summary>
+        protected override void InitializeCulture()
+        {
+            base.InitializeCulture();
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var listaSelecao = ((ListBox)sender).SelectedValue;
+
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo(listaSelecao);
+
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(listaSelecao);
+
+            var retorno = GetGlobalResourceObject("Resource", "AccCode");
+            traducao.Text = retorno.ToString();
+        }
+    }
+}
